e8326ee3d20cc1477484beb219bede73b49e6875
Kozerema <kozeremavika@gmail.com>
2023-05-11 17:59:59 +0300

    Initial commit

---
+++ src/main/java/ua/com/searchauto/SearchAutoApplication.java
@@ -0,0 +1,13 @@
+package ua.com.searchauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/searchauto/SearchAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.searchauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
